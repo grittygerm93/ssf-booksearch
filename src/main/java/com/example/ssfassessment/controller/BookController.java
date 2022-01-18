9578ae1ca8f396f83bf8a6a7d4283a69d5599794
Alex <alexius.lzc@gmail.com>
2022-01-18 14:58:52 +0800

    final cleanup

--- src/main/java/com/example/ssfassessment/controller/BookController.java
+++ src/main/java/com/example/ssfassessment/controller/BookController.java
@@ -6,10 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/book/works")
--- src/main/java/com/example/ssfassessment/controller/SearchController.java
+++ src/main/java/com/example/ssfassessment/controller/SearchController.java
@@ -5,9 +5,9 @@ import com.example.ssfassessment.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -53,27 +53,5 @@ public class SearchController {
         model.addAttribute("list", list);
         return "linkspage";
     }
-
-    //only use multivaluemap if posted from a form
-   /* @PostMapping(path= "/{id}" ,params = "action=update")
-    public String convert(Model model,
-                          @RequestBody MultiValueMap<String, String> form,
-                          @RequestParam(name="units", defaultValue="metrics") String units,
-                          @RequestParam MultiValueMap<String, String> queryParams,
-                          @PathVariable(name="city", required=true) String city) {
-
-        model.addAttribute("person", new Contacts());
-        return "";
-    }*/
-
-    //redirecting and accepting file uploads
-/*    @PostMapping("/sendfile")
-    public String postItem(Model model, @RequestParam MultipartFile file, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("message",
-                "%s was uploaded".formatted(file.getOriginalFilename()));
-
-        return "redirect:/";
-    }*/
-
 
 }
